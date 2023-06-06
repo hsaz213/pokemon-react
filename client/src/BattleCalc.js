21d04b6b7ab4fe373d71437b8e3ee742f951ca85
Sandor Hajdu <125272342+hsaz213@users.noreply.github.com>
2023-06-06 10:05:34 +0200

    set turns

--- client/src/BattleCalc.js
+++ client/src/BattleCalc.js
@@ -1,35 +1,56 @@
 import React, { useState, useEffect } from 'react';
 
-function PokemonBattle({ pokemonName }) {
-    const [pokemonData, setPokemonData] = useState(null);
+function PokemonBattle({ myPokemonName, enemyPokemonName }) {
+    const [myPokemonData, setMyPokemonData] = useState(null);
+    const [enemyPokemonData, setEnemyPokemonData] = useState(null);
+    const [turn, setTurn] = useState('mine');
 
     useEffect(() => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        fetch(`https://pokeapi.co/api/v2/pokemon/${myPokemonName}`)
             .then(response => response.json())
-            .then(data => setPokemonData(data))
+            .then(data => setMyPokemonData(data))
             .catch(err => console.error(err));
-    }, [pokemonName]);
+    }, [myPokemonName]);
+
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${enemyPokemonName}`)
+            .then(response => response.json())
+            .then(data => setEnemyPokemonData(data))
+            .catch(err => console.error(err));
+    }, [enemyPokemonName]);
 
     const calculateDamage = (attackerAttack, defenderDefense) => {
         const Z = Math.random() * (255 - 217) + 217;
         return ((((2 / 5 + 2) * attackerAttack * 60 / defenderDefense) / 50) + 2) * Z / 255;
     }
 
-    if (pokemonData) {
-        const hp = pokemonData.stats[0].base_stat;
-        const attack = pokemonData.stats[1].base_stat;
-        const defense = pokemonData.stats[2].base_stat;
+    const handleTurn = () => {
+        setTurn(prevTurn => prevTurn === 'mine' ? 'enemy' : 'mine');
+    }
 
-        const damage = calculateDamage(attack, defense);
+    if (myPokemonData && enemyPokemonData) {
+        const attacker = turn === 'mine' ? myPokemonData : enemyPokemonData;
+        const defender = turn === 'mine' ? enemyPokemonData : myPokemonData;
+
+        const attackerAttack = attacker.stats[1].base_stat;
+        const defenderDefense = defender.stats[2].base_stat;
+
+        const damage = calculateDamage(attackerAttack, defenderDefense);
 
         return (
             <div>
-                <h2>{pokemonData.name}</h2>
-                <img src={pokemonData.sprites.front_default} alt="Pixel avatar" />
-                <img src={pokemonData.sprites["official-artwork"].front_default} alt="Large avatar" />
-                <p>HP: {hp}</p>
-                <p>Attack: {attack}</p>
-                <p>Defense: {defense}</p>
+                <h2>{turn === 'mine' ? `${myPokemonData.name} attacks!` : `${enemyPokemonData.name} attacks!`}</h2>
+                <button onClick={handleTurn}>Next Turn</button>
+                <h3>Attacker: {attacker.name}</h3>
+                <img src={attacker.sprites.front_default} alt="Pixel avatar" />
+                <img src={attacker.sprites["official-artwork"].front_default} alt="Large avatar" />
+                <p>Attack: {attackerAttack}</p>
+
+                <h3>Defender: {defender.name}</h3>
+                <img src={defender.sprites.front_default} alt="Pixel avatar" />
+                <img src={defender.sprites["official-artwork"].front_default} alt="Large avatar" />
+                <p>Defense: {defenderDefense}</p>
+
                 <p>Damage: {damage}</p>
             </div>
         );
@@ -38,4 +59,4 @@ function PokemonBattle({ pokemonName }) {
     }
 }
 
-export default BattleCalc;
+export default PokemonBattle;
