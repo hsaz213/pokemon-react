0b88052bf9a22d04cfaf81ef812330d0c642aad7
Bonifert <116727997+Bonifert@users.noreply.github.com>
2023-06-08 12:18:12 +0200

    start game

--- client/src/StartGame.jsx
+++ client/src/StartGame.jsx
@@ -1,16 +1,20 @@
 import React from "react";
+import gigachu from "./img/start.png";
 
 export default function StartGame({ onStart }) {
   return (
     <div className="startGame">
       <div>
-        <h1 className="productName">Pokemon meg kitalaljuk</h1>
+        <h1 className="productName">
+          Pokemon Annihilator 3000<sup>®</sup>
+        </h1>
         <button className="playButton" onClick={onStart}>
-          Start your adventure!
+          Start your bloody adventure! :)
         </button>
         <h2 className="teamName">
-          A Geekachu production<sup>®</sup>
+          A Geekachu<sup>™</sup> production
         </h2>
+        <img src={gigachu} className="gigachu" alt="Gigachu" />
       </div>
     </div>
   );
